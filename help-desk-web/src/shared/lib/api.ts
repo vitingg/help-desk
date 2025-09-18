4856a2317ff66e0a492ecef51d71a6a02d1a4737
Victor Gabriel <victorgabel99@gmail.com>
2025-09-18 15:38:44 -0300

    fix: variable on vite

--- help-desk-web/src/shared/lib/api.ts
+++ help-desk-web/src/shared/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const apiUrl = import.meta.env.VITE_API_URL;
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: apiUrl,
   timeout: 5000,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
